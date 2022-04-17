7d222577beb87527017cdb613de59ff3bbda57da
Igor <zakatov0303@gmail.com>
2022-04-17 22:07:29 +0300

    Created new screens
    Created TestsFragment and SettingsFragment
    These fragments added to profile graph

---
+++ app/src/main/java/com/example/testsys/screens/TestsFragment.java
@@ -0,0 +1,14 @@
+package com.example.testsys.screens;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.testsys.R;
+import com.example.testsys.databinding.TestsFragmentBinding;
+
+public class TestsFragment extends Fragment {
+    private TestsFragmentBinding binding;
+
+    public TestsFragment() {
+        super(R.layout.tests_fragment);
+    }
+}
