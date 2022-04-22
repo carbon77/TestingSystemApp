cc5db8e67655f3a54a03d26255566ce21cd11ee4
Igor <zakatov0303@gmail.com>
2022-04-22 12:09:17 +0300

    Renamed CreateTestFragment ot TestFormFragment

---
+++ app/src/main/java/com/example/testsys/screens/TestFormFragment.java
@@ -0,0 +1,14 @@
+package com.example.testsys.screens;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.testsys.R;
+import com.example.testsys.databinding.TestFormFragmentBinding;
+
+public class TestFormFragment extends Fragment {
+    private TestFormFragmentBinding binding;
+
+    public TestFormFragment() {
+        super(R.layout.test_form_fragment);
+    }
+}
