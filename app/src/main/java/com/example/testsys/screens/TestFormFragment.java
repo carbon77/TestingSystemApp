bdadc93ce1cbe203ceeb405c510cdd6917b5bdc7
Igor <zakatov0303@gmail.com>
2022-04-22 15:58:51 +0300

    Implemented test creation
    Added args to TestFormFragment: testId, userId

--- app/src/main/java/com/example/testsys/screens/TestFormFragment.java
+++ app/src/main/java/com/example/testsys/screens/TestFormFragment.java
@@ -3,17 +3,36 @@ package com.example.testsys.screens;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.testsys.R;
 import com.example.testsys.databinding.TestFormFragmentBinding;
+import com.example.testsys.models.test.Test;
+import com.example.testsys.models.user.UserViewModel;
+import com.example.testsys.utils.DateService;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class TestFormFragment extends Fragment {
+    private NavController navController;
     private TestFormFragmentBinding binding;
+    private Test currentTest;
+    private UserViewModel userViewModel;
+    private String testId;
+    private String userId;
 
     public TestFormFragment() {
         super(R.layout.test_form_fragment);
@@ -24,10 +43,59 @@ public class TestFormFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         binding = TestFormFragmentBinding.bind(view);
         setHasOptionsMenu(true);
+
+        NavHostFragment navHost = (NavHostFragment) requireActivity().getSupportFragmentManager().findFragmentById(R.id.main_nav_host_fragment);
+        navController = navHost.getNavController();
+
+        testId = TestFormFragmentArgs.fromBundle(getArguments()).getTestId();
+        userId = TestFormFragmentArgs.fromBundle(getArguments()).getUserId();
+
+        if (testId == null) {
+            binding.etTestVersion.setText("1");
+            Calendar date = new GregorianCalendar();
+            binding.etTestCreateDate.setText(DateService.fromCalendar(date));
+            binding.etTestModificationDate.setText(DateService.fromCalendar(date));
+        }
     }
 
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         inflater.inflate(R.menu.test_form_toolbar_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_test_item:
+                saveTest();
+                break;
+        }
+
+        return false;
+    }
+
+    private void saveTest() {
+        if (binding.etTestText.getText().toString().equals("")) {
+            binding.etTestText.setError("This field is required!");
+            Toast.makeText(requireContext(), "Check the form", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String text = binding.etTestText.getText().toString();
+        int version = Integer.parseInt(binding.etTestVersion.getText().toString());
+        String creationDate = binding.etTestCreateDate.getText().toString();
+        String modificationDate = binding.etTestModificationDate.getText().toString();
+
+        currentTest = new Test(null, userId, text, version, creationDate, modificationDate);
+
+        DatabaseReference testRef = FirebaseDatabase.getInstance().getReference("tests").push();
+        testRef.setValue(currentTest);
+        testId = testRef.getKey();
+        currentTest.setId(testId);
+
+        DatabaseReference userTestRef = FirebaseDatabase.getInstance().getReference("userTest").child(userId);
+        userTestRef.child(testId).setValue(true);
+
+        navController.navigateUp();
     }
 }
--- app/src/main/java/com/example/testsys/screens/TestsFragment.java
+++ app/src/main/java/com/example/testsys/screens/TestsFragment.java
@@ -6,15 +6,18 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDirections;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.testsys.R;
 import com.example.testsys.databinding.TestsFragmentBinding;
+import com.example.testsys.models.user.UserViewModel;
 
 public class TestsFragment extends Fragment {
     private TestsFragmentBinding binding;
+    private String userId;
 
     public TestsFragment() {
         super(R.layout.tests_fragment);
@@ -26,12 +29,17 @@ public class TestsFragment extends Fragment {
         binding = TestsFragmentBinding.bind(view);
 
         binding.btnTestForm.setOnClickListener(this::btnTestFormClick);
+
+        UserViewModel userViewModel = new ViewModelProvider(requireActivity()).get(UserViewModel.class);
+        userViewModel.getUser().observe(getViewLifecycleOwner(), user -> {
+            userId = user.getUid();
+        });
     }
 
     private void btnTestFormClick(View view) {
         NavHostFragment navHost = (NavHostFragment) requireActivity().getSupportFragmentManager().findFragmentById(R.id.main_nav_host_fragment);
         NavController navController = navHost.getNavController();
-        NavDirections action = TabsFragmentDirections.actionTabsFragmentToTestFormFragment();
+        NavDirections action = TabsFragmentDirections.actionTabsFragmentToTestFormFragment(null, userId);
         navController.navigate(action);
     }
 }
