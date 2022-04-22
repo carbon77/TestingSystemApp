bd18be3c1138f9adf338becf4a8c1b2658a78a4b
Igor <zakatov0303@gmail.com>
2022-04-22 12:02:54 +0300

    Changed navigation bar
    Renamed NavFragment to TabsFragment
    Toobar is in MainActivity now
    Implemented working navigation bar in MainActivity

--- app/src/main/java/com/example/testsys/MainActivity.java
+++ app/src/main/java/com/example/testsys/MainActivity.java
@@ -1,27 +1,120 @@
 package com.example.testsys;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.NavGraph;
 import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.testsys.databinding.ActivityMainBinding;
+import com.example.testsys.screens.TabsFragment;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private final int[] NO_ACTION_BAR_DESTINATIONS = {
+            R.id.sign_in_fragment,
+            R.id.sign_up_fragment
+    };
 
     private ActivityMainBinding binding;
     private NavController navController;
-    private NavHostFragment navHost;
+
+    private FragmentManager.FragmentLifecycleCallbacks fragmentListener = new FragmentManager.FragmentLifecycleCallbacks() {
+        @Override
+        public void onFragmentViewCreated(@NonNull FragmentManager fm, @NonNull Fragment f, @NonNull View v, @Nullable Bundle savedInstanceState) {
+            super.onFragmentViewCreated(fm, f, v, savedInstanceState);
+
+            if (f instanceof TabsFragment || f instanceof NavHostFragment) return;
+            onNavControllerActivated(NavHostFragment.findNavController(f));
+        }
+    };
+
+    private NavController.OnDestinationChangedListener destinationListener = (navController, navDestination, bundle) -> {
+        ActionBar supportActionBar = getSupportActionBar();
+
+        if (supportActionBar == null) return;
+
+        for (int id : NO_ACTION_BAR_DESTINATIONS) {
+            if (id == navDestination.getId()) {
+                supportActionBar.hide();
+                break;
+            } else {
+                supportActionBar.show();
+            }
+        }
+
+        supportActionBar.setTitle(navDestination.getLabel());
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
 
-        navHost = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.main_nav_host_fragment);
-        navController = navHost.getNavController();
-        getSupportActionBar().hide();
+        getSupportFragmentManager().registerFragmentLifecycleCallbacks(fragmentListener, true);
+
+        navController = getRootNavController();
+        navController.addOnDestinationChangedListener(destinationListener);
+        onNavControllerActivated(navController);
+    }
+
+    @Override
+    protected void onDestroy() {
+        getSupportFragmentManager().unregisterFragmentLifecycleCallbacks(fragmentListener);
+        navController = null;
+        super.onDestroy();
+    }
+
+    private NavController getRootNavController() {
+        NavHostFragment navHost = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.main_nav_host_fragment);
+        return navHost.getNavController();
+    }
+
+    private void onNavControllerActivated(NavController navController) {
+        if (this.navController == navController) return;
+        this.navController.removeOnDestinationChangedListener(destinationListener);
+        navController.addOnDestinationChangedListener(destinationListener);
+        this.navController = navController;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return navController.navigateUp() || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (isStartDestination(navController.getCurrentDestination())) {
+            super.onBackPressed();
+        } else {
+            navController.popBackStack();
+        }
+    }
+
+    private boolean isStartDestination(NavDestination currentDestination) {
+        if (currentDestination == null) return false;
+
+        NavGraph graph = currentDestination.getParent();
+        if (graph == null) return false;
+
+        List<Integer> startDestinations = Arrays.asList(
+                R.id.tabs_fragment,
+                R.id.sign_in_fragment,
+                graph.getStartDestinationId()
+        );
+
+        return startDestinations.contains(currentDestination.getId());
     }
 }
--- app/src/main/java/com/example/testsys/screens/TabsFragment.java
+++ app/src/main/java/com/example/testsys/screens/TabsFragment.java
@@ -5,34 +5,32 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
-import androidx.navigation.NavHost;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.testsys.R;
-import com.example.testsys.databinding.NavFragmentBinding;
+import com.example.testsys.databinding.TabsFragmentBinding;
 
-public class NavFragment extends Fragment {
-    private NavFragmentBinding binding;
+public class TabsFragment extends Fragment {
+    private TabsFragmentBinding binding;
     private NavController navController;
     private NavHostFragment navHostFragment;
 
-    public NavFragment() {
-        super(R.layout.nav_fragment);
+    public TabsFragment() {
+        super(R.layout.tabs_fragment);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding = NavFragmentBinding.bind(view);
+        binding = TabsFragmentBinding.bind(view);
         navHostFragment = (NavHostFragment) getChildFragmentManager().findFragmentById(R.id.profile_nav_host_fragment);
         navController = navHostFragment.getNavController();
 
-        NavigationUI.setupWithNavController(binding.toolbar, navController);
         NavigationUI.setupWithNavController(binding.bottomNavigation, navController);
     }
 }
