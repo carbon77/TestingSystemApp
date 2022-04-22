e3918f9c27abdfeb33545b385aca41e00397c2d4
Igor <zakatov0303@gmail.com>
2022-04-22 22:40:31 +0300

    Added username validation
    Added validation for unique usernames

--- app/src/main/java/com/example/testsys/models/user/UserService.java
+++ app/src/main/java/com/example/testsys/models/user/UserService.java
@@ -2,12 +2,16 @@ package com.example.testsys.models.user;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 /*
 * UserService class has static methods to work with signed in user in Firebase
 * */
 public class UserService {
+    static private final FirebaseDatabase db = FirebaseDatabase.getInstance();
+    static private final DatabaseReference dbRef = db.getReference();
+
     public static void loadCurrentUser(UserListener completeListener) {
         FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         if (firebaseUser == null) {
@@ -40,18 +44,36 @@ public class UserService {
 
     public static void signUp(String email, String password, String username, UserListener completeListener) {
         FirebaseAuth auth = FirebaseAuth.getInstance();
-        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+
+        dbRef.child("usernames").child(username).get().addOnCompleteListener(t -> {
+            if (t.isSuccessful()) {
+                completeListener.invoke(null);
+                return;
+            }
+
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                    String id = firebaseUser.getUid();
+                    User user = new User(null, email, username, username);
+                    createUserModel(id, user, completeListener);
+                } else {
+                    completeListener.invoke(null);
+                }
+            });
+        });
+    }
+
+    private static void createUserModel(String id, User user, UserListener completeListener) {
+        dbRef.child("users").child(id).setValue(user).addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
-                FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-                String id = firebaseUser.getUid();
-                User user = new User(null, email, username, username);
-                FirebaseDatabase.getInstance().getReference().child("users").child(id).setValue(user).addOnCompleteListener(t -> {
-                    if (t.isSuccessful()) {
-                        user.setId(id);
-                        completeListener.invoke(user);
-                    } else {
-                        completeListener.invoke(null);
-                    }
+                user.setId(id);
+                dbRef.child("usernames").child(user.getUsername()).setValue(id).addOnCompleteListener(t -> {
+                   if (t.isSuccessful()) {
+                       completeListener.invoke(user);
+                   } else {
+                       completeListener.invoke(null);
+                   }
                 });
             } else {
                 completeListener.invoke(null);
