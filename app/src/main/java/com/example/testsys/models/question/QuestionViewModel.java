e79aea2777705ed28da49dbfe040b33548bcfa55
Igor <zakatov0303@gmail.com>
2022-04-30 15:22:53 +0300

    Created QuestionViewModel

---
+++ app/src/main/java/com/example/testsys/models/question/QuestionViewModel.java
@@ -0,0 +1,27 @@
+package com.example.testsys.models.question;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.List;
+
+public class QuestionViewModel extends ViewModel {
+    private MutableLiveData<List<Question>> questions;
+    private final String testId;
+
+    public QuestionViewModel(String testId) {
+        super();
+        this.testId = testId;
+    }
+
+    public LiveData<List<Question>> getQuestions() {
+        if (questions == null) {
+            QuestionService.getQuestions(testId, it -> {
+                questions.setValue(it);
+            });
+        }
+
+        return questions;
+    }
+}
