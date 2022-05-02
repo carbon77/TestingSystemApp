d7846721020123128498212ed16ec6c059d4dc3b
Igor <zakatov0303@gmail.com>
2022-05-02 10:54:59 +0300

    Updated Question model
    Created inner class Answer
    Changed type of field Answer to List<Answer>

--- app/src/main/java/com/example/testsys/models/question/Question.java
+++ app/src/main/java/com/example/testsys/models/question/Question.java
@@ -1,14 +1,14 @@
 package com.example.testsys.models.question;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Question {
     private String id;
     private String testId;
     private String text;
     private QuestionType type;
-    private Map<String, Boolean> answers;
+    private List<Answer> answers;
 
     public Question() {
 
@@ -17,16 +17,15 @@ public class Question {
     public Question(String text, QuestionType type) {
         this.text = text;
         this.type = type;
-        answers = new HashMap<>();
+        answers = new ArrayList<>();
     }
 
-    public void addAnswer(String answer, boolean isRight) {
-        answers.put(answer, isRight);
+    public void addAnswer(String text, boolean correct) {
+        answers.add(new Answer(text, correct));
     }
 
-    public void setAnswer(int index, boolean newRight) {
-        String key = (String) answers.keySet().toArray()[0];
-        answers.put(key, newRight);
+    public void setAnswer(int index, boolean newCorrect) {
+        answers.get(index).setCorrect(newCorrect);
     }
 
     public String getId() {
@@ -65,7 +64,37 @@ public class Question {
         this.text = text;
     }
 
-    public Map<String, Boolean> getAnswers() {
+    public List<Answer> getAnswers() {
         return answers;
+    }
+
+    public static class Answer {
+        private String text;
+        private boolean correct;
+
+        public Answer() {
+
+        }
+
+        public Answer(String text, boolean correct) {
+            this.text = text;
+            this.correct = correct;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+
+        public boolean isCorrect() {
+            return correct;
+        }
+
+        public void setCorrect(boolean correct) {
+            this.correct = correct;
+        }
     }
 }
