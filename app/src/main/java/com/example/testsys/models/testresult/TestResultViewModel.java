22966c044c55a2ba8eefbe8fd12a1ac45e716028
Igor <zakatov0303@gmail.com>
2022-05-07 17:52:10 +0300

    Changed TestResultViewModel

--- app/src/main/java/com/example/testsys/models/testresult/TestResultViewModel.java
+++ app/src/main/java/com/example/testsys/models/testresult/TestResultViewModel.java
@@ -1,10 +1,22 @@
 package com.example.testsys.models.testresult;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import java.util.function.Consumer;
 
 public class TestResultViewModel extends ViewModel {
+    private MutableLiveData<TestResult> testResult = new MutableLiveData<>();
+
+    public LiveData<TestResult> getTestResult() {
+        return testResult;
+    }
+
+    public void updateTestResult(TestResult testResult) {
+        this.testResult.setValue(testResult);
+    }
+
     public void createTestResult(TestResult testResult, Consumer<TestResult> completeListener) {
         TestResultService.createTestResult(testResult, completeListener);
     }
