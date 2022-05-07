384ba118a95254b405cc97baf2bb403a02a02dbe
Igor <zakatov0303@gmail.com>
2022-05-07 22:18:38 +0300

    Added calculating scores and creation test result

--- app/src/main/java/com/example/testsys/screens/test/TestPassFragment.java
+++ app/src/main/java/com/example/testsys/screens/test/TestPassFragment.java
@@ -7,11 +7,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.testsys.R;
 import com.example.testsys.databinding.TestPassFragmentBinding;
 import com.example.testsys.models.ProgressTestViewModel;
+import com.example.testsys.models.question.Answer;
 import com.example.testsys.models.question.Question;
+import com.example.testsys.models.question.QuestionType;
 import com.example.testsys.models.question.QuestionViewModel;
 import com.example.testsys.models.test.Test;
 import com.example.testsys.models.test.TestViewModel;
@@ -23,6 +26,7 @@ import com.example.testsys.models.user.UserViewModel;
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.List;
+import java.util.Map;
 
 public class TestPassFragment extends Fragment {
     private TestPassFragmentBinding binding;
@@ -117,6 +121,13 @@ public class TestPassFragment extends Fragment {
             binding.testPassTabs.getTabAt(progress).select();
         });
 
+        binding.btnTestPassFinish.setOnClickListener(v -> {
+            calculateScores();
+            testResultViewModel.createTestResult(result, ts -> {
+                NavHostFragment.findNavController(this).navigateUp();
+            });
+        });
+
         binding.testPassTabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
@@ -133,6 +144,47 @@ public class TestPassFragment extends Fragment {
 
             }
         });
+    }
+
+    private void calculateScores() {
+        int scores = 0;
+        List<TestResult.TestResultQuestion> resultQuestions = result.questionsToArray();
+
+        for (int i = 0; i < resultQuestions.size(); i++) {
+            int questionScores = 0;
+            TestResult.TestResultQuestion resQ = resultQuestions.get(i);
+            Question question = questions.get(i);
+
+            if (resQ.getType() == QuestionType.RADIO) {
+                for (Map.Entry<String, Answer> entry : resQ.getAnswers().entrySet()) {
+                    Answer questionAnswer = question.getAnswers().get(entry.getKey());
+                    if (entry.getValue().getCorrect() && entry.getValue().getCorrect() == questionAnswer.getCorrect()) {
+                        questionScores += question.getScore();
+                    }
+                }
+            } else if (resQ.getType() == QuestionType.CHECKBOX) {
+                int countCorrectAnswers = 0;
+                int countCorrectOptions = 0;
+
+                for (Map.Entry<String, Answer> entry : resQ.getAnswers().entrySet()) {
+                    Answer questionAnswer = question.getAnswers().get(entry.getKey());
+                    if (questionAnswer.getCorrect()) {
+                        countCorrectOptions++;
+                    }
+
+                    if (entry.getValue().getCorrect() && entry.getValue().getCorrect() == questionAnswer.getCorrect()) {
+                        countCorrectAnswers++;
+                    }
+                }
+
+                questionScores += (question.getScore() * countCorrectAnswers) / countCorrectOptions;
+            }
+
+            resQ.setScore(questionScores);
+            scores += questionScores;
+        }
+
+        result.setTotalScores(scores);
     }
 
     private void initTabs() {
