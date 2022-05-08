4b0c6bf40c30fbb622735fb440157d5ea86d6485
Igor <zakatov0303@gmail.com>
2022-05-08 14:23:52 +0300

    Created test preview fragment content
    Created layout
    Relocate to screens.test.preview package

---
+++ app/src/main/java/com/example/testsys/screens/test/preview/TestPreviewFragment.java
@@ -0,0 +1,44 @@
+package com.example.testsys.screens.test.preview;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.testsys.R;
+import com.example.testsys.databinding.TestPreviewFragmentBinding;
+import com.example.testsys.models.testresult.TestResult;
+import com.example.testsys.models.testresult.TestResultViewModel;
+
+import java.util.List;
+
+public class TestPreviewFragment extends Fragment {
+    TestPreviewFragmentBinding binding;
+    TestResultViewModel testResultViewModel;
+    TestResult testResult;
+    List<TestResult.TestResultQuestion> questions;
+
+    public TestPreviewFragment() {
+        super(R.layout.test_preview_fragment);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding = TestPreviewFragmentBinding.bind(view);
+        testResultViewModel = new ViewModelProvider(requireActivity()).get(TestResultViewModel.class);
+        testResultViewModel.getTestResult().observe(getViewLifecycleOwner(), testResult -> {
+            this.testResult = testResult;
+            this.questions = testResult.questionsToArray();
+
+            TestReviewAdapter adapter = new TestReviewAdapter(questions);
+            binding.testResultRecyclerView.setAdapter(adapter);
+            binding.testResultRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        });
+    }
+}
