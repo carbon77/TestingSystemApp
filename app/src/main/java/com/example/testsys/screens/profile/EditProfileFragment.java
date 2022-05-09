eac88d38461b944c0f2d405fd303eacbdc9b9b00
Igor <zakatov0303@gmail.com>
2022-05-09 20:37:46 +0300

    Added edit profile fragment content

--- app/src/main/java/com/example/testsys/screens/profile/EditProfileFragment.java
+++ app/src/main/java/com/example/testsys/screens/profile/EditProfileFragment.java
@@ -6,12 +6,18 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.testsys.R;
 import com.example.testsys.databinding.EditProfileFragmentBinding;
+import com.example.testsys.models.user.User;
+import com.example.testsys.models.user.UserViewModel;
 
 public class EditProfileFragment extends Fragment {
-    EditProfileFragmentBinding binding;
+    private EditProfileFragmentBinding binding;
+    private UserViewModel userViewModel;
+    private User user;
 
     public EditProfileFragment() {
         super(R.layout.edit_profile_fragment);
@@ -21,5 +27,17 @@ public class EditProfileFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         binding = EditProfileFragmentBinding.bind(view);
+        userViewModel = new ViewModelProvider(requireActivity()).get(UserViewModel.class);
+
+        userViewModel.getUser().observe(getViewLifecycleOwner(), user -> {
+            this.user = user;
+
+            binding.etDisplayName.setText(user.getDisplayName());
+            binding.etEmail.setText(user.getEmail());
+        });
+
+        binding.btnCancel.setOnClickListener(v -> {
+            NavHostFragment.findNavController(this).navigateUp();
+        });
     }
 }
